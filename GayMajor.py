d1203a158e8ad2c568b6f5b375a4255dcc1a6d4d
mint2y <43726065+mint2y@users.noreply.github.com>
2021-07-13 19:48:25 +0100

    Update GayMajor.py

--- GayMajor.py
+++ GayMajor.py
@@ -1,5 +1,14 @@
-def major():
+IsMajorGay = input("Is Major Gay ")
+if IsMajorGay == 'no':
+    print("Wrong :(")
 
 
+while IsMajorGay == 'no':
+    IsMajorGay = input("Is Major Gay ")
 
+    if IsMajorGay == 'no':
+        print("Wrong :(")
+
+    if IsMajorGay == 'yes':
+        print("You are correct :)")
 
